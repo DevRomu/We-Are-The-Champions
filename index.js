260a611a99d01b9da23c826672facb727e424f3e
Ro <orinarhomill@gmail.com>
2024-04-21 00:53:37 -0500

    Trying to figure out how to append items

--- index.js
+++ index.js
@@ -1,1 +1,68 @@
-// javascript
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
+import {
+  getDatabase,
+  ref,
+  push,
+  onValue,
+  remove,
+} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
+
+const databaseURLKey = {
+  databaseURL: "https://we-are-the-champion-app-default-rtdb.firebaseio.com/",
+};
+
+const app = initializeApp(databaseURLKey);
+const db = getDatabase(app);
+const endorsementsListDB = ref(db, "Endorsements");
+
+const inputFieldEl = document.getElementById("input-field");
+const addButtonEl = document.getElementById("publish");
+const endorsementsListsEL = document.getElementById("comment-lists");
+
+function clearInputFieldEl() {
+  inputFieldEl.value = "";
+}
+
+function clearEndorsementsEl() {
+  endorsementsListsEL.innerHTML = "";
+}
+
+addButtonEl.addEventListener("click", function () {
+  let inputValue = inputFieldEl.value;
+  push(endorsementsListDB, inputValue);
+
+  console.log(inputValue);
+
+  clearInputFieldEl();
+});
+
+onValue(endorsementsListDB, function (snapshot) {
+  let endorsementsArray = Object.entries(snapshot.val());
+
+  //   for (let i = 0; i < endorsementsArray.length; i++) {
+  //     let currentEndorsement = endorsementsArray[i];
+  //     let currentEndorsementID = currentEndorsement[0];
+  //     let currentEndorsementValue = currentEndorsement[1];
+
+  //     appendEndorsementsList(currentEndorsement);
+  //   }
+
+  clearEndorsementsEl();
+
+  for (let i = 0; i < endorsementsArray.length; i++) {
+    appendEndorsementsList(endorsementsArray[i]);
+  }
+});
+
+function appendEndorsementsList(endorsements) {
+  endorsementsListsEL.innerHTML += `<li>${itemValue}</li>`;
+  //   let endorsementsListID = endorsements[0];
+  //   let endorsementsListValue = endorsements[1];
+  //   let endorsementsListEl = document.createElement("li");
+  //   endorsementsListEl.textContent = endorsementsListValue;
+  //   endorsementsListEl.addEventListener("click", function () {
+  //     let extractionsListElInDB = ref(db, `Endorsements/${endorsementsListID}`);
+  //     remove(extractionsListElInDB);
+  //   });
+  //   endorsementsListsEL.appendChild(endorsementsListEl);
+}
